b1cfb7a17b0b67fab6c8b51be6f46fb295db5735
Awa Desmoline <awadesmoline@gmail.com>
2020-08-11 10:17:49 +0100

    Create an index page with products and payment gateways

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,12 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    products = Product.all
+    @products_purchase = products.where(stripe_plan_name:nil, paypal_plan_name:nil)
+    @products_subscription = products - @products_purchase
+  end
+
+  def submit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/', to: 'orders#index'
+  post '/orders/submit', to: 'orders#submit'
 end
